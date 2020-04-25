a9c84c3acfe10420f4524fba36d625a719e5d68a
Jahanvi Patel <jahanvi316@gmail.com>
2020-04-25 18:59:49 -0400

    Add files via upload

--- questionOne.py
+++ questionOne.py
@@ -47,38 +47,50 @@ with open("testing.txt") as testing:
         
 #find missing values (1.00000000000000e+99) and replacing it
 def missingValues(array): 
-    print("missingValues called")
-    for r in range(len(array)): #loop through array - columns
-        for c in range(len(array[r])): #loop through array -rows
+    #print("missingValues called")
+    for r in range(len(array)): #loop through array - row
+        for c in range(len(array[r])): #loop through array -column
             print("for loop: ")
             print(r, c)
             if array[r][c] == ("1.00000000000000e+99"):  #find 1.00000000000000e+99 (missing entry)
-                print("if statement in missingValues successful")
+               # print("if statement in missingValues successful")
                 findSimilarRow(array, r) 
-    print("missingValues end")
+    #print("missingValues end")
     return array;
     
 #find patterns for the row of missing value    
 def findSimilarRow(array, currentRow):
-    print("findSimilarRow called")
+    #print("findSimilarRow called")
     for r in range(len(array)):
         if r != currentRow:
-            print("if statement in findSimilarRow successful")
+            #print("if statement in findSimilarRow successful")
             colToFill = isRowTheSame(array[currentRow], array[r]) #check if rows are the same
             if len(colToFill) != 0: #if length is 0, then assume rows not equal, otherwise fill missing columns
                 fillMissingColumns(array, currentRow, r, colToFill)#fill all missing columns here
+                return array;
 
 #method to find if the rows are the same for missing value rows; returns array of indexes of columns of missing values if same                
 def isRowTheSame(missingValRow, comparedRow):
-    print("isRowTheSame called")
+    #print("isRowTheSame called")
     colToFillArray = []
+    #print("comparedRow is")
+    #print(comparedRow)
     for i in range(len(comparedRow)):
+        #print("for loop in isRowTheSame called for ")
+        #print(i)
         if missingValRow[i] != 1.00000000000000e+99: #if number is not 1.00000000000000e+99
+            print("missingValRow isn't number")
             if missingValRow[i] != comparedRow[i]: #if the rows are not the same
+                print("missingValRow isn't same as comparedRow")
                 return [] #return empty array if not the same
+            else:
+                print("missingValRow same as comparedRow")
         else:
+            print("missingValRow is number")
             colToFillArray.append(i) #if it is the missing value (1.00000000000000e+99), add index to array colToFill
     print("isRowTheSame Ended")
+    print("colToFillArray :")
+    print(colToFillArray)
     return colToFillArray
     
 
@@ -87,7 +99,7 @@ def fillMissingColumns(array, currentRow, row, colToFill):
     for i in len(colToFill):
         currentCol = colToFill[i]
         array[currentRow][currentCol] = array[row][currentCol]
-        return array;
+    return array;
   
 #method to classify
 def classify():
